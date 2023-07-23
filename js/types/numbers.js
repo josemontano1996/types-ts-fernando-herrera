58176ed1c8613ba21c89a85b7b12db1351edbc31
Jose Manuel Montano Mengual <josemanuel1996enologia@gmail.com>
2023-07-23 09:24:16 +0200

    second commit

---
+++ js/types/numbers.js
@@ -0,0 +1,11 @@
+"use strict";
+(function () {
+    var avengers = 10;
+    var villians = 20;
+    if (avengers < villians) {
+        console.log('Estamos en problemas');
+    }
+    else {
+        console.log('Nos salvamos');
+    }
+})();
